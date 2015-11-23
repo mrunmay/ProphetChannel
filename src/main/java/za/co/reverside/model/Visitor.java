63c6c79566c4f438acab985d70aed59db3c78446
Manmay <manmay.e.mohanty@gmail.com>
2015-11-23 21:43:17 +0200

    Adding test.html

--- src/main/java/za/co/reverside/model/Visitor.java
+++ src/main/java/za/co/reverside/model/Visitor.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Date;
 
 @Entity
 @Table(name = "visitors")
@@ -15,6 +16,9 @@ public class Visitor
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     Long id;
+
+    @Column(name="dov")
+    Date dateOfVisit;
 
     @Column(name = "status")
     Boolean status;
